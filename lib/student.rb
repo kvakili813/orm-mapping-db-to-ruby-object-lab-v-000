e6cce4da973eed7e61e077951a4cee871be9af81
Kayvan Vakili <kvakili813@gmail.com>
2018-07-10 20:48:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,17 +19,23 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    self.new_from_db(DB[:conn].execute(sql, name).flatten)
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
